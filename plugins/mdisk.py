4d86c8647d378bce35cee293c02271d51bdc873b
zaidkilleryt <123814688+zaidkilleryt@users.noreply.github.com>
2023-02-20 17:26:13 +0530

    Update mdisk.py

--- plugins/mdisk.py
+++ plugins/mdisk.py
@@ -1,6 +1,6 @@
-# Not available in this repo use @UploadLinkToFileBot this bot support mdisk link ✅💯
+# Not available in this repo use @iaszaidyt this bot support mdisk link ✅💯
 # Only paid user support mdisk link ✅
 # Per month just $0.50/₹10 💸
-# Bot username @UploadLinkToFileBot 🤖
+# Owner username @iaszaidyt 🤖
 # Check bot /plans and upgrade your subscription
 # Please donate you can send any amount 💰
