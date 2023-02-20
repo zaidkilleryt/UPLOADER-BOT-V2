4f89b0740c3a31659e904d0216d3ea3de9a8944c
zaidkilleryt <123814688+zaidkilleryt@users.noreply.github.com>
2023-02-20 16:53:13 +0530

    Update startmsg.py

--- plugins/startmsg.py
+++ plugins/startmsg.py
@@ -20,12 +20,12 @@ Or use /cancel to terminate incomplete processes."""
 You can now send a new URL."""
     NO_PROCESS_FOUND = """🤷‍♂️ No pending uploads were found. You can upload files by sending a link now!"""
     FORMAT_SELECTION = "👇𝗦𝗲𝗹𝗲𝗰𝘁 𝗔𝗻𝗱 𝗖𝗵𝗼𝘀𝗲 𝗬𝗼𝘂𝗿 𝗙𝗼𝗿𝗺𝗮𝘁👇"
-    SET_CUSTOM_USERNAME_PASSWORD = """\n👮‍♂ Powered By :</b> @LISA_FAN_LK"""
+    SET_CUSTOM_USERNAME_PASSWORD = """\n👮‍♂ Powered By :</b> @iaszaidyt"""
     DOWNLOAD_START = "📥 DOWNLOADING..."
     UPLOAD_START = "📤 UPLOADING..."
     RCHD_TG_API_LIMIT = "<b>Downloaded in:</b> {} seconds.\n<b>Detected file size:</b> {}.\n\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations 😕."
     #AFTER_SUCCESSFUL_UPLOAD_MSG = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰\n\n@NT_BOT_CHANNEL"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "𝘛𝘏𝘈𝘕𝘒𝘚 𝘍𝘖𝘙 𝘜𝘚𝘐𝘕𝘎 𝘔𝘌 🥰\n\n@za_updates"
     SAVED_CUSTOM_THUMB_NAIL = "Save Your Thumbnail ✅."
     DEL_ETED_CUSTOM_THUMB_NAIL = " Delete Your Thumbnail ✅."
     CUSTOM_CAPTION_UL_FILE = "{}"
@@ -47,17 +47,17 @@ You can now send a new URL."""
 
 6. Comments /about /viewthumbnail /info
 """
-    ABOUT_TEXT = """<b>🔘 My Name :</b> URL Uploader Bot V2 🚀
+    ABOUT_TEXT = """<b>🔘 My Name :</b> Doraemon Url Uploader 🚀
 
-<b>🔘 Channel :</b> <a href="https://t.me/NT_BOT_CHANNEL">NT BOT</a>
+<b>🔘 Channel :</b> <a href="https://t.me/za_updates">ZA UPDATE</a>
 
-<b>🔘 Source :</b> <a href="https://github.com/LISA-KOREA/UPLOADER-BOT-V2">Click Here</a>
+<b>🔘 Source :</b> <a href="https://youtube.com/@zaidalihacker">Click Here</a>
 
 <b>🔘 Language :</b> <a href="https://www.python.org/">Python 3.10.8</a>
 
 <b>🔘 Framework :</b> <a href="https://docs.pyrogram.org/">Pyrogram 1.4.16</a>
 
-<b>🔘 Creater :</b> @LISA_FAN_LK"""
+<b>🔘 Creater :</b> @iaszaidyt"""
 
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Send your thumbnail pic to generate custom thumbail."
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
